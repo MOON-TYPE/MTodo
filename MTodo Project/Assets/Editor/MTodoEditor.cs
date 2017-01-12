07c43062b3e0692906fc3ed191cdf98ae31d9290
lPinchol <pinchodev91@gmail.com>
2017-01-12 14:24:54 +0100

    Init Proyect

---
+++ MTodo Project/Assets/Editor/MTodoEditor.cs
@@ -0,0 +1,20 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//                                                                              \\
+// MTodoEditor.cs (00/00/0000)													\\
+// Autor: Antonio Mateo (Moon Pincho) 									        \\
+// Descripcion:																	\\
+// Fecha Mod:		00/00/0000													\\
+// Ultima Mod:																	\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+#endregion
+
+public class MTodoEditor : MonoBehaviour 
+{
+
+
+}
