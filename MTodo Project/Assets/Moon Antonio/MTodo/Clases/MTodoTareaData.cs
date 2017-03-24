7075bb6caaa17778ca990a1c92b1d8e114e2d949
Moon Antonio <pinchodev91@gmail.com>
2017-03-24 18:43:09 +0100

    Agregado MTODOTareaData.cs

---
+++ MTodo Project/Assets/Moon Antonio/MTodo/Clases/MTodoTareaData.cs
@@ -0,0 +1,39 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// MTodoTareaData.cs (24/03/2017)												\\
+// Autor: Antonio Mateo (Moon Antonio) 									        \\
+// Descripcion:		Data de tareas de MTodo										\\
+// Fecha Mod:		24/03/2017													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+#endregion
+
+namespace MoonAntonio.MTodo
+{
+	/// <summary>
+	/// <para>Data de tareas de MTodo</para>
+	/// </summary>
+	[Serializable]
+	public class MTodoTareaData : ScriptableObject
+	{
+		#region Variables
+        /// <summary>
+        /// <para>Tareas de MTodo</para>
+        /// </summary>
+        public List<clsMTodoTareas> Tareas = new List<clsMTodoTareas>();					// Tareas de MTodo
+        /// <summary>
+        /// <para>Categorias de las tareas de MTodo</para>
+        /// </summary>
+        public List<string> Categorias = new List<string>(){ "Default", "Urgente" };            // Categorias de las tareas de MTodo
+        /// <summary>
+        /// <para>Ruta de data</para>
+        /// </summary>
+        public string RutaDataMTodoTareas = "";                                             // Ruta de data
+		#endregion
+	}
+}
