670e87eaea8e92fde7ad748a6337f2d1df18624a
Moon Antonio <pinchodev91@gmail.com>
2017-03-24 18:25:49 +0100

    Agregado clsMTodoTareas.cs

---
+++ MTodo Project/Assets/Moon Antonio/MTodo/Clases/clsMTodoTareas.cs
@@ -0,0 +1,24 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// clsMTodoTareas.cs (24/03/2017)												\\
+// Autor: Antonio Mateo (Moon Antonio) 									        \\
+// Descripcion:		Clase de las tareas de MTodo								\\
+// Fecha Mod:		24/03/2017													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace MoonAntonio.MTodo
+{
+	/// <summary>
+	/// <para>Clase de las tareas de MTodo</para>
+	/// </summary>
+	[System.Serializable]
+	public class clsMTodoTareas
+	{
+
+	}
+}
